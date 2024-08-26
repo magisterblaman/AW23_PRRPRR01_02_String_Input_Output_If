8f4d809a41ed817a77f95b7cae44a0cb1b9c373f
hannesblaman <hannes@blaman.com>
2024-08-26 13:48:32 +0200

    Grundläggande output + strings

--- AW23_PRRPRR01_02_String_Input_Output_If/Program.cs
+++ AW23_PRRPRR01_02_String_Input_Output_If/Program.cs
@@ -7,6 +7,24 @@ using System.Threading.Tasks;
 namespace AW23_PRRPRR01_02_String_Input_Output_If {
 	internal class Program {
 		static void Main(string[] args) {
+			string myText = "Hejsan svejsan";
+
+			Console.WriteLine(myText);
+
+			myText = "Hejdå";
+
+			Console.WriteLine(myText);
+
+			string firstName = "Hannes";
+
+			string lastName = "Blåman";
+
+			string fullName = firstName + " " + lastName;
+
+			Console.WriteLine(fullName);
+
+			// Console.Write("Hejsan");
+			// Console.WriteLine("SIODHuisdhad");
 		}
 	}
 }
